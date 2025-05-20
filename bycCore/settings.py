bd7a4be7efea4049736055f908144a3724c6b064
KARG <karg95@outlook.com>
2025-05-20 16:42:47 -0400

    Creacion app de gen_settings, declaración de apps en el settings y cambio de lenguaje y zona horaria

--- bycCore/settings.py
+++ bycCore/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_home',
+    'gen_settings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
