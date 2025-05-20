9a73abf9ad7bc16ecdb020bba9957787f74b6ce3
KARG <karg95@outlook.com>
2025-05-20 16:36:23 -0400

    Configuración de la DB en PostgreSQL

--- bycCore/settings.py
+++ bycCore/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'bycCore.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bycCoreDB",
+        "USER": "postgres",
+        "PASSWORD": "123456",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
